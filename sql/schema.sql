ee96062b8d967e1d0d244d269b52f88069312723
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2023-12-10 16:19:43 +0900

    feat: add schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,21 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
 );
+
+CREATE TABLE items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    stock INT NOT NULL
+);
+
+CREATE TABLE user_items (
+    user_id INT NOT NULL,
+    item_id INT NOT NULL,
+    quantity INT NOT NULL,
+    purchase_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, item_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (item_id) REFERENCES items(id)
+);
